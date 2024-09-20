3c5d5ce673914e222cc4cd7620e3e7fae588da31
lMatkovic <lmatkovic00@gmail.com>
2024-09-20 23:54:12 +0200

    ispravak

--- NogometniKlubAPP/NogometniKlubAPP/Data/NogometniKlubContext.cs
+++ NogometniKlubAPP/NogometniKlubAPP/Data/NogometniKlubContext.cs
@@ -3,16 +3,21 @@ using NogometniKlubAPP.Models;
 
 namespace NogometniKlubAPP.Data
 {
-    public class NogometniKlubContext : DbContext
-    {
-        public NogometniKlubContext(DbContextOptions<NogometniKlubContext> opcije) : base(opcije) 
+    
+        public class NogometniKlubContext : DbContext
         {
-            
-        }
+            public NogometniKlubContext(DbContextOptions<NogometniKlubContext> opcije) : base(opcije)
+            {
 
-        public DbSet<Klub> Klubovi { get; set; }
-        public DbSet<Igrac> Igraci { get; set; }
-        public DbSet<Trener> Treneri { get; set; }
-        public DbSet<Utakmica> Utakmice { get; set; }
-    }
+            }
+
+
+            public DbSet<Klub> Klubovi { get; set; }
+            public DbSet<Igrac> Igraci { get; set; }
+            public DbSet<Trener> Treneri { get; set; }
+            public DbSet<Utakmica> Utakmice { get; set; }
+
+
+        }
+    
 }
