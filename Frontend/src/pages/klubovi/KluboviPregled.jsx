d0c688eebf621f2373e2d15310c1ca754fbcc970
Luka Matković <lmatkovic00@gmail.com>
2024-10-10 14:13:38 +0200

    Dodavanje tablica

--- Frontend/src/pages/klubovi/KluboviPregled.jsx
+++ Frontend/src/pages/klubovi/KluboviPregled.jsx
@@ -1,13 +1,22 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import KlubService from "../../services/KlubService"
+import { Table } from "react-bootstrap"
 
 
 export default function KluboviPregled(){
+
+    const[klubovi, setKlubovi] = useState()
     
     async function dohvatiKlubove(){
-        await KlubService.get()
+      const odgovor = await KlubService.get()
+      if(odgovor.greska){
+        alert(odgovor.poruka)
+        return
+      }
+        setKlubovi(odgovor.poruka)
     }
-
+    
+    
     useEffect(()=>{
         dohvatiKlubove()
     },[])
@@ -16,7 +25,47 @@ export default function KluboviPregled(){
     
     return (
         <>
-        Ovdje će doći pregled klubova
+        <Table striped bordered hover responsive>
+            <thead>
+                <tr>
+                    <th>Naziv</th>
+                    <th>Osnovan</th>
+                    <th>Stadion</th>
+                    <th>Predsjednik</th>
+                    <th>Država</th>
+                    <th>Liga</th>
+                    <th>Akcija</th>
+                </tr>
+            </thead>
+            <tbody>
+                {klubovi && klubovi.map((klub,index)=>(
+                    <tr key={index}>
+                        <td>
+                            {klub.naziv}
+                        </td>
+                        <td>
+                            {klub.osnovan}
+                        </td>
+                        <td>
+                            {klub.stadion}
+                        </td>
+                        <td>
+                            {klub.predsjednik}
+                        </td>
+                        <td>
+                            {klub.drzava}
+                        </td>
+                        <td>
+                            {klub.liga}
+                        </td>
+                        <td>Akcija</td>
+
+                    </tr>
+
+                ))}
+            </tbody>
+
+        </Table>
         </>
     )
 }
